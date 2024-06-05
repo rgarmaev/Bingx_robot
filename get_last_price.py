253f303b2a9526b34cf9d01d07bc3b7753df9367
R.Garmaev <garmaev.rinchin@gmail.com>
2024-06-05 17:06:35 +0300

    update_last_price

--- get_last_price.py
+++ get_last_price.py
@@ -7,40 +7,53 @@ APIURL = "https://open-api.bingx.com"
 APIKEY = "EuW8fOL9il9ywUiI1qjzb06WkgwbOIGfIZANTcLOq8QHj4jfVsnGykgqZRdBRdfBjUZpigiLoyfRnj3rMSw"
 SECRETKEY = "35MrDlIprD0zPupEaTKG1xwCxGlvVMGKAUAu0A2wBuDuUtcoACwLPGBRbXK3H20sT4YoD6srm17XjCTVtUr8g"
 
+
 def demo():
     payload = {}
     path = '/openApi/swap/v1/ticker/price'
     method = "GET"
     paramsMap = {
-    "timestamp": "1702718923479",
-    "symbol": "BTC-USDT"
-}
+        "symbol": "BTC-USDT"
+    }
     paramsStr = parseParam(paramsMap)
-    return send_request(method, path, paramsStr, payload)
+    response = send_request(method, path, paramsStr, payload)
+
+    # Проверяем наличие поля 'data' и 'price' в ответе
+    if 'data' in response and 'price' in response['data']:
+        latest_price = response['data']['price']
+        current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+        return f'{current_time}, {paramsMap["symbol"]}, {latest_price}'
+    else:
+        # Выводим полный ответ для отладки
+        print("Полный ответ:", response)
+        return "Не удалось получить последнюю цену."
+
 
 def get_sign(api_secret, payload):
     signature = hmac.new(api_secret.encode("utf-8"), payload.encode("utf-8"), digestmod=sha256).hexdigest()
-    print("sign=" + signature)
     return signature
 
 
 def send_request(method, path, urlpa, payload):
-    url = "%s%s?%s&signature=%s" % (APIURL, path, urlpa, get_sign(SECRETKEY, urlpa))
-    print(url)
+    url = f"{APIURL}{path}?{urlpa}&signature={get_sign(SECRETKEY, urlpa)}"
     headers = {
         'X-BX-APIKEY': APIKEY,
     }
     response = requests.request(method, url, headers=headers, data=payload)
-    return response.text
+
+    # Преобразуем ответ в JSON
+    response_json = response.json()
+    return response_json
+
 
 def parseParam(paramsMap):
     sortedKeys = sorted(paramsMap)
-    paramsStr = "&".join(["%s=%s" % (x, paramsMap[x]) for x in sortedKeys])
-    if paramsStr != "":
-     return paramsStr+"&timestamp="+str(int(time.time() * 1000))
+    paramsStr = "&".join([f"{x}={paramsMap[x]}" for x in sortedKeys])
+    if paramsStr:
+        return paramsStr + "&timestamp=" + str(int(time.time() * 1000))
     else:
-     return paramsStr+"timestamp="+str(int(time.time() * 1000))
+        return "timestamp=" + str(int(time.time() * 1000))
 
 
 if __name__ == '__main__':
-    print("demo:", demo())
+    print(demo())
